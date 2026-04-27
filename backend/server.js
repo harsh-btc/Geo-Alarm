743003aa22c6b6f1d99786ea48512e9b92bf5bae
Harsh <harshrajawat908@gmail.com>
2026-04-27 10:40:01 +0530

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -13,21 +13,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({
-  origin: function(origin, callback) {
-    const allowed = [
-      'http://localhost:5173',
-      process.env.CLIENT_URL,
-    ].filter(Boolean);
-    
-    if (!origin || allowed.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-}));
+app.use(cors({ origin: '*' }));
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
